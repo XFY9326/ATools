a307d31270ecdcf9c37343e6d6d00cb2d6df7f62
XFY9326 <1069665464@qq.com>
2022-03-11 09:05:03 +0000

    Add inline for coroutines methods

--- library/src/main/java/io/github/xfy9326/aio/utils/Coroutines.kt
+++ library/src/main/java/io/github/xfy9326/aio/utils/Coroutines.kt
@@ -4,12 +4,12 @@ package io.github.xfy9326.aio.utils
 
 import kotlinx.coroutines.*
 
-suspend fun <T> withIOContext(parentJob: Job? = null, block: suspend CoroutineScope.() -> T): T =
+suspend inline fun <T> withIOContext(parentJob: Job? = null, noinline block: suspend CoroutineScope.() -> T): T =
     withContext(Dispatchers.IO + SupervisorJob(parentJob), block)
 
-suspend fun <T> runIOJob(printStackTrace: Boolean = true, block: suspend CoroutineScope.() -> T): Result<T> =
+suspend inline fun <T> runIOJob(printStackTrace: Boolean = true, crossinline block: suspend CoroutineScope.() -> T): Result<T> =
     withIOContext {
         runCatching { block() }.also {
-            if(printStackTrace) it.exceptionOrNull()?.printStackTrace()
+            if (printStackTrace) it.exceptionOrNull()?.printStackTrace()
         }
     }
