bcbb2bb538fd2fa793792adc7ce8fad987651f8b
XFY9326 <1069665464@qq.com>
2022-03-11 09:01:51 +0000

    Change param name for runIOJob

--- library/src/main/java/io/github/xfy9326/aio/utils/Coroutines.kt
+++ library/src/main/java/io/github/xfy9326/aio/utils/Coroutines.kt
@@ -7,9 +7,9 @@ import kotlinx.coroutines.*
 suspend fun <T> withIOContext(parentJob: Job? = null, block: suspend CoroutineScope.() -> T): T =
     withContext(Dispatchers.IO + SupervisorJob(parentJob), block)
 
-suspend fun <T> runIOJob(reportError: Boolean = true, block: suspend CoroutineScope.() -> T): Result<T> =
+suspend fun <T> runIOJob(printStackTrace: Boolean = true, block: suspend CoroutineScope.() -> T): Result<T> =
     withIOContext {
         runCatching { block() }.also {
-            if(reportError) it.exceptionOrNull()?.printStackTrace()
+            if(printStackTrace) it.exceptionOrNull()?.printStackTrace()
         }
     }
