c845c54fbab15082d93ef38e4b93efd69d866d04
XFY9326 <1069665464@qq.com>
2022-03-11 10:37:18 +0000

    Add File text writer

--- library/src/main/java/io/github/xfy9326/aio/okio/Text.kt
+++ library/src/main/java/io/github/xfy9326/aio/okio/Text.kt
@@ -3,17 +3,31 @@
 package io.github.xfy9326.aio.okio
 
 import android.net.Uri
+import okio.sink
+import okio.source
+import java.io.File
 import java.io.IOException
 
 @Throws(IOException::class)
-fun Uri.writeText(text: String, mode: String? = null): Boolean =
-    sink(mode).useBuffer {
+fun Uri.writeText(text: String, append: Boolean = false): Unit =
+    sink(if (append) "wa" else "w").useBuffer {
         writeUtf8(text)
-        true
     }
 
 @Throws(IOException::class)
 fun Uri.readText(): String =
+    source().useBuffer {
+        readUtf8()
+    }
+
+@Throws(IOException::class)
+fun File.writeText(text: String, append: Boolean = false): Unit =
+    sink(append).useBuffer {
+        writeUtf8(text)
+    }
+
+@Throws(IOException::class)
+fun File.readText(): String =
     source().useBuffer {
         readUtf8()
     }
