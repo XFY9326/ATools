4e72a5bd28c7016ea92632f4ab1b6e327843ad2f
XFY9326 <1069665464@qq.com>
2022-03-11 01:28:15 +0000

    Add test activity

---
+++ app/src/main/java/io/github/xfy9326/demo/aio/MainActivity.kt
@@ -0,0 +1,12 @@
+@file:Suppress("RedundantOverride")
+
+package io.github.xfy9326.demo.aio
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
