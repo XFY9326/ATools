4d3ab0929de27d479804a5814767191bb5279c35
XFY9326 <1069665464@qq.com>
2022-04-15 15:59:03 +0100

    Add Mutex withTryLock

--- library/src/main/java/io/github/xfy9326/aio/utils/Coroutines.kt
+++ library/src/main/java/io/github/xfy9326/aio/utils/Coroutines.kt
@@ -3,6 +3,7 @@
 package io.github.xfy9326.aio.utils
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.sync.Mutex
 
 suspend inline fun <T> withIOContext(parentJob: Job? = null, noinline block: suspend CoroutineScope.() -> T): T =
     withContext(Dispatchers.IO + SupervisorJob(parentJob), block)
@@ -13,3 +14,15 @@ suspend inline fun <T> runIOJob(printStackTrace: Boolean = true, crossinline blo
             if (printStackTrace) it.exceptionOrNull()?.printStackTrace()
         }
     }
+
+@Throws(IllegalStateException::class)
+inline fun <T> Mutex.withTryLock(owner: Any? = null, crossinline action: () -> T): T? {
+    if (tryLock(owner)) {
+        try {
+            return action()
+        } finally {
+            unlock(owner)
+        }
+    }
+    return null
+}
