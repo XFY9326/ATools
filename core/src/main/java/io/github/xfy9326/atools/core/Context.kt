e69037e73c439ebff1ae36d2830dd4d83d4165e3
xfy9326 <xfy9326@qq.com>
2022-06-24 14:39:00 +0100

    Add Context.isPermissionGranted

--- core/src/main/java/io/github/xfy9326/atools/core/Context.kt
+++ core/src/main/java/io/github/xfy9326/atools/core/Context.kt
@@ -1,7 +1,12 @@
 package io.github.xfy9326.atools.core
 
 import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
 import io.github.xfy9326.atools.core.startup.AToolsInitializer
 
 val AppContext: Context
     get() = AToolsInitializer.applicationContext
+
+fun Context.isPermissionGranted(permission: String): Boolean =
+    ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
