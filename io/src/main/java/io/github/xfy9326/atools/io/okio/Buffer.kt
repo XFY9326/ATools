875a97f45399592f505984cb58b234c5ab8d9196
xfy9326 <xfy9326@qq.com>
2022-06-24 16:33:51 +0100

    Remove crossinline on useBuffer

--- io/src/main/java/io/github/xfy9326/atools/io/okio/Buffer.kt
+++ io/src/main/java/io/github/xfy9326/atools/io/okio/Buffer.kt
@@ -5,9 +5,9 @@ package io.github.xfy9326.atools.io.okio
 import okio.*
 import kotlin.io.use
 
-inline fun <T> Source.useBuffer(crossinline block: BufferedSource.() -> T) = buffer().use(block)
+inline fun <T> Source.useBuffer(block: BufferedSource.() -> T) = buffer().use(block)
 
-inline fun <T> Sink.useBuffer(flush: Boolean = true, crossinline block: BufferedSink.() -> T) = buffer().use {
+inline fun <T> Sink.useBuffer(flush: Boolean = true, block: BufferedSink.() -> T) = buffer().use {
     try {
         it.block()
     } finally {
