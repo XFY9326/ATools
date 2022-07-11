beeff381f7485aec3da268aff495c5d252fd23ed
xfy9326 <xfy9326@qq.com>
2022-07-11 14:14:27 +0100

    Fix kotlin version error

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "7.2.1" apply false
     id("com.android.library") version "7.2.1" apply false
-    kotlin("android") version "1.7.10" apply false
+    kotlin("android") version "1.7.0" apply false
     kotlin("jvm") version "1.7.0" apply false
 }
 
