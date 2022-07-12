07a0769362aa386dde1ee9ee29a2564cfb5592d3
xfy9326 <xfy9326@qq.com>
2022-07-12 04:58:18 +0100

    Add uri support in AssetFile

--- io/src/main/java/io/github/xfy9326/atools/io/file/AssetFile.kt
+++ io/src/main/java/io/github/xfy9326/atools/io/file/AssetFile.kt
@@ -3,12 +3,16 @@
 package io.github.xfy9326.atools.io.file
 
 import android.content.res.AssetManager
+import android.net.Uri
 import io.github.xfy9326.atools.io.IOManager
 import java.io.IOException
 
 fun assetFile(path: String) = AssetFile(path)
 
 class AssetFile(val path: String) {
+    val uri: Uri
+        get() = Uri.parse("file:///android_asset/$path")
+
     @Throws(IOException::class)
     fun open(accessMode: Int = AssetManager.ACCESS_STREAMING) = IOManager.assetManager.open(path, accessMode)
 
